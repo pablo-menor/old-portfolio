26b0aea343722d360e2ee0f55ab6a4dafc641417
Pablo <pablomenor2001@gmail.com>
2021-07-22 17:35:07 +0200

    added cards function

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 window.onload = function(){
+
+    function githubEffect(id){
+        const g = document.querySelector(`.im${id}`);
+        const p = document.querySelector(`.p${id}`);  
     
-    const gitub_icon = document.querySelectorAll('.github');
-    const source = document.querySelectorAll('.see-source');  
+    
+            g.addEventListener('mouseover', (e) =>{
+                g.style.transition = "0.5s ease";
+                p.style.transition = "0.8s ease";
+                g.style.transform = "translateX(-74px)";
+                p.style.transform = "scale(1)";
+    
+                p.addEventListener('mouseout', () =>{
+                    g.style.transition = "1s ease";
+                    p.style.transition = "0.8s ease";
+                    g.style.transform = "translateX( 0px)";
+                    p.style.transform = "scale(0)";
+    
+                })        
+            }) 
+            console.log(id);      
+    }
 
-    gitub_icon.forEach(icon =>{
-        icon.addEventListener('mouseover', () =>{
-            icon.style.transform = "translateX(-70px)";
-            source.forEach(pp =>{
-                pp.style.transform = "scale(1)";
-
-                pp.addEventListener('mouseout', () =>{
-                    icon.style.transform = "translateX( 0px)";
-                    pp.style.transform = "scale(0)";
-
-                })
-            })
-           
-        })
-        
-    })
+    githubEffect('1');
+    githubEffect('2');
+    githubEffect('3');
+ 
+   
 
 }
