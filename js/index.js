35faf2ca0b666ce967cd2b482362df1d093a403a
Pablo <pablomenor2001@gmail.com>
2021-07-26 01:04:27 +0200

    added stuff

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 window.onload = function(){
 
-    //Animation for the project cards github icon.
+
+    const welcome = document.querySelector("#welcome");
+
+    welcome.style.marginLeft = '6.3%';
+
+    const intro = document.querySelector("#intro");
+
+    intro.style.marginLeft = '0%';
+
+    setTimeout(() => {
+        intro.style.color = '#b9b7b7';
+    }, 1500);
+    //-----------------------Animation for the project cards github icon.-----------------
     function githubEffect(id){
         const g = document.querySelector(`.im${id}`);
         const p = document.querySelector(`.p${id}`);  
@@ -26,16 +38,22 @@ window.onload = function(){
     githubEffect('2');
     githubEffect('3');
 
-    //Animation for the rocket
+    //----------------Animation for the rocket------------------------------
     const rocket = document.querySelector("#rocket");
     const space = document.querySelector("#space");
+    const text_space = document.querySelector("#text-space");
 
     space.addEventListener("mouseover", () =>{
         rocket.style.animation = "leave 4s";
         setTimeout(function(){
-            rocket.style.display = "none";
+            rocket.style.display = "none";          
         },2000);
-    
+
+        setTimeout(function(){  
+            text_space.style.display = "block";
+            
+        },1000);
+       
         
     })
 }
